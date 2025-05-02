3a51dcc5f082fd8274b9e56541c68aa0f659fb35
Vladikw <vladamihajlova988@gmail.com>
2025-05-03 01:52:34 +0700

    Добавила авторизацию

--- nerpinar/apps/main/views.py
+++ nerpinar/apps/main/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def tickets(request):
+    return render(request, 'main/tickets.html')
+
